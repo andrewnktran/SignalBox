3498a86d839ed6ba5215515b9cbe2ee1aa28ea9d
Andrew Tran <andrewnktran@gmail.com>
2025-05-18 11:05:09 -0500

    Add --binary flag to log raw telemetry packets to binary file

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,7 @@ int main(int argc, char* argv[]) {
     bool appendMode = false;
     bool userProvidedLog = false;
     int sensorFilter = -1;  // -1 means no filter
+    bool binaryMode = false;
 
     // Parse CLI arguments
     for (int i = 1; i < argc; ++i) {
@@ -34,6 +35,9 @@ int main(int argc, char* argv[]) {
         else if (arg.rfind("--sensor=", 0) == 0) {
             sensorFilter = std::stoi(arg.substr(9));
         }
+        else if (arg == "--binary") {
+            binaryMode = true;
+        }
         else {
             std::cerr << "Unknown option: " << arg << std::endl;
             return 1;
@@ -45,17 +49,21 @@ int main(int argc, char* argv[]) {
         auto now = std::chrono::system_clock::now();
         std::time_t t = std::chrono::system_clock::to_time_t(now);
         std::stringstream ss;
-        ss << "log_" << std::put_time(std::localtime(&t), "%Y%m%d_%H%M%S") << ".csv";
+        ss << (binaryMode ? "telemetry_" : "log_")
+            << std::put_time(std::localtime(&t), "%Y%m%d_%H%M%S")
+            << (binaryMode ? ".bin" : ".csv");
         logFileName = ss.str();
     }
 
     std::ofstream logfile;
-    if (appendMode) {
-        logfile.open(logFileName, std::ios::app);
+    if (binaryMode) {
+        logfile.open(logFileName, std::ios::binary | (appendMode ? std::ios::app : std::ios::trunc));
     }
     else {
-        logfile.open(logFileName);
-        logfile << "timestamp,sensor_id,value\n";
+        logfile.open(logFileName, appendMode ? std::ios::app : std::ios::trunc);
+        if (!appendMode) {
+            logfile << "timestamp,sensor_id,value\n";
+        }
     }
 
     int written = 0;
@@ -71,9 +79,16 @@ int main(int argc, char* argv[]) {
             << ", Sensor ID: " << pkt.sensor_id
             << ", Value: " << pkt.value << std::endl;
 
-        logfile << pkt.timestamp << "," << pkt.sensor_id << "," << pkt.value << "\n";
-        ++written;
+        if (binaryMode) {
+            logfile.write(reinterpret_cast<const char*>(&pkt.timestamp), sizeof(pkt.timestamp));
+            logfile.write(reinterpret_cast<const char*>(&pkt.sensor_id), sizeof(pkt.sensor_id));
+            logfile.write(reinterpret_cast<const char*>(&pkt.value), sizeof(pkt.value));
+        }
+        else {
+            logfile << pkt.timestamp << "," << pkt.sensor_id << "," << pkt.value << "\n";
+        }
 
+        ++written;
         std::this_thread::sleep_for(std::chrono::milliseconds(500));
     }
 
