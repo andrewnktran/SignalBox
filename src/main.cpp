54c917a1993843a773bfb27f8c79c1809eded4b8
Andrew Tran <andrewnktran@gmail.com>
2025-05-18 10:44:15 -0500

    Add CLI flags for --count and --log file name

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,36 @@
 #include <fstream>
 #include <thread>
 #include <chrono>
+#include <string>
+#include <cstdlib>
 #include "parser.h"
 
-int main() {
+int main(int argc, char* argv[]) {
     std::cout << "SignalBox Starting...\n";
 
-    std::ofstream logfile("telemetry_log.csv");
+    // Defaults
+    int count = 10;
+    std::string logFileName = "telemetry_log.csv";
+
+    // Parse CLI args
+    for (int i = 1; i < argc; ++i) {
+        std::string arg(argv[i]);
+        if (arg.rfind("--count=", 0) == 0) {
+            count = std::stoi(arg.substr(8));
+        }
+        else if (arg.rfind("--log=", 0) == 0) {
+            logFileName = arg.substr(6);
+        }
+        else {
+            std::cerr << "Unknown option: " << arg << std::endl;
+            return 1;
+        }
+    }
+
+    std::ofstream logfile(logFileName);
     logfile << "timestamp,sensor_id,value\n";
 
-    for (int i = 0; i < 10; ++i) {
+    for (int i = 0; i < count; ++i) {
         TelemetryPacket pkt = generatePacket();
 
         std::cout << "Timestamp: " << pkt.timestamp
